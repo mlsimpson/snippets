13b4eb116fdf3a1347714a32870391f92188b851
mlsimpson <maui@threv.net>
2022-02-04 15:03:03 -0500

    refactored potential_palindrome as O(n)

--- potential_palindrome.py
+++ potential_palindrome.py
@@ -11,9 +11,10 @@ def palindrome(word):
         char = char.lower()
         word_dict.setdefault(char, 0)
         word_dict[char] += 1
-    for letter in word_dict:
-        if word_dict[letter] % 2 != 0:
+        if word_dict[char] % 2 != 0:
             odd_count += 1
+        else:
+            odd_count -= 1
     if odd_count > 1:
         print("Can't be a palindrome")
         return
