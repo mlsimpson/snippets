230edbaa52e43f4d72a4e68228f3bafceeaa5e4d
mlsimpson <maui@threv.net>
2022-02-06 15:33:55 -0500

    refactored potential_palindrome

--- potential_palindrome.py
+++ potential_palindrome.py
@@ -1,26 +1,31 @@
 def palindrome(word):
     if not word:
         print("Can't be a palindrome")
-        return
+        return False
+
     odd_count = 0
     word_dict = {}
+
     for char in word:
         if not char.isalpha():
             print("Can't be a palindrome")
-            return
+            return False
+
         char = char.lower()
         word_dict.setdefault(char, 0)
         word_dict[char] += 1
+
         if word_dict[char] % 2 != 0:
             odd_count += 1
         else:
             odd_count -= 1
+
     if odd_count > 1:
         print("Can't be a palindrome")
-        return
+        return False
 
     print("Can be a palindrome")
-    return
+    return True
 
 palindrome("code") # -> no
 palindrome("aab") # -> yes
